7bf0af1312d1ec23754a0f8f081859c984556053
flyespace <51199699+flyespace@users.noreply.github.com>
2024-12-05 15:15:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,8 @@
 #
 
 # Modify default IP
+#sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
