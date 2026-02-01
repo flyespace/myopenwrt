f3db4db81b446c67357cbde3a1e775f1cfa09978
flyespace <51199699+flyespace@users.noreply.github.com>
2026-02-01 13:22:33 +0800

    Comment out hostname and password modification commands
    Comment out sed commands for modifying hostname and password.

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,3 +8,6 @@ sed -i 's/192.168.1.1/192.168.5.208/g' package/base-files/luci2/bin/config_gener
 
 # Modify hostname
 sed -i 's/OpenWrt/FlyEspace/g' package/base-files/files/bin/config_generate
+
+# Modify default password to 16897168
+# sed -i "s/root:::0:99999:7:::/root:$(openssl passwd -1 '16897168'):0:99999:7:::/" package/base-files/files/etc/shadow
